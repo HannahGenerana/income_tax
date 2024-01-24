25699197d3947437342248676b861e46b6350057
HannahGenerana <hannahgenerana@gmail.com>
2024-01-25 00:38:57 +0800

    pseudocode

---
+++ income_tax.py
@@ -0,0 +1,22 @@
+# Exercise 12: Calculate income tax for the given income by adhering to the rules below
+# Taxable Income	Rate (in %)
+# First $10,000      	0
+# Next $10,000	       10
+# The remaining	       20
+
+# pseudocode
+
+# create a function to consider each rule
+
+# print the given income
+
+# check the conditions
+   # income is less than 10000
+
+   # income is less than 20000
+
+   # applying the rules to the income
+
+# print the result
+
+# print the result of the example
